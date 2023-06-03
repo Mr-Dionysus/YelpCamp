778387a1217cf2bcc8cffbb09da85eed8797c897
Denis <dbreznikov@gmail.com>
2023-06-03 17:00:10 +0300

    Added some Bootstrap on pages

--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,14 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
+            image: "https://source.unsplash.com/collection/483251",
+            description:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias corporis impedit magnam cupiditate iusto in eius sunt error, id ad autem a tempore placeat itaque nam voluptatibus voluptate qui reprehenderit.",
+            price,
         });
         await camp.save();
     }
